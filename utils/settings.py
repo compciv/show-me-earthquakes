fd00bc7749d66363ef3a647213f63c07c85008ce
Dan Nguyen <dansonguyen@gmail.com>
2016-02-18 12:24:17 -0800

    s

--- utils/settings.py
+++ utils/settings.py
@@ -1,4 +1,4 @@
-from os.path import join
+from os.path import join, realpath
 
 # directories
 DATA_DIR = 'tempdata'
@@ -15,7 +15,7 @@ WRANGLED_DATA_FILENAME = join(DATA_DIR, 'wrangled-data.csv')
 GEOCODER_CREDENTIALS_FILENAME = 'creds_mapzen.txt'
 
 # A place to throw a webpage on the local hard drive
-LOCAL_WEBPAGE_PATH = os.path.realpath(join(DATA_DIR, "myfunwebpage.html"))
+LOCAL_WEBPAGE_PATH = realpath(join(DATA_DIR, "myfunwebpage.html"))
 
 
 USGS_QUAKE_EVENT_BASEURL = "http://earthquake.usgs.gov/earthquakes/eventpage/"
